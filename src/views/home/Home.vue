5294b9e6c9725c24b046351c344d898ae0743bb7
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2018-12-25 11:33:30 +0900

    first commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,6 @@
+<template src="./home.pug"></template>
+
+<script src="./home.ts" lang="ts"></script>
+
+<style src="./home.scss" lang="scss" scoped></style>
+
---
+++ src/views/home/home.ts
@@ -0,0 +1,5 @@
+import { Component, Vue } from "vue-property-decorator";
+import AWS from "aws-sdk";
+
+@Component({})
+export default class Home extends Vue {}
